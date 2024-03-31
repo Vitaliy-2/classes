027fc305337372397e56194a1e42952a7df467d7
Vitaliy <Vitaliy@yandex.ru>
2024-03-31 10:53:13 +0300

    lesson_25 - __call__

--- Записи/lesson___25(11.03).py
+++ Записи/lesson___25(11.03).py
@@ -31,122 +31,29 @@ __call__ - вызов объекта как функции
 from functools import total_ordering
 
 
-@total_ordering
-class Kettlebell:
-    """
-    Гиря
-    """
-
-    def __init__(self, weight, length=40, width=10):
-        self.weight = weight
-        self.length = length
-        self.width = width
-
-    def __str__(self) -> str:
-        return f'Гиря весом {self.weight} кг'
-
-    def __bool__(self) -> bool:
-        return self.weight > 0
-
-    def __len__(self) -> int:
-        return self.length
-
-    def __eq__(self, other: 'Kettlebell') -> bool:  # other - другой экземпляр этого класса
-        """
-        Равенство
-        :param other:
-        :return:
-        """
-        # Проверка на принадлежность к классу
-        if not isinstance(other, Kettlebell):
-            return NotImplemented
-        return self.weight == other.weight
-
-    def __lt__(self, other: 'Kettlebell') -> bool:
-        """
-        Меньше
-        :param other:
-        :return:
-        """
-        if not isinstance(other, Kettlebell):
-            return NotImplemented
-        return self.weight < other.weight
+class Cities:
+    cities = ['Москва', 'Новосибирск', 'Самара', 'Екатеринбург']
 
 
-class Dumbbells:
-    def __eq__(self, other):
-        return NotImplemented
-    pass
+class Game:
+    def __init__(self, cities: Cities):  # Это Агрегация - сбор данных из вне
+        self.Cities = cities
+
+    def print_cities(self):
+        print(self.Cities.cities)
 
 
-# ket1 = Kettlebell(16, 45, 11)
-# print(ket1)
-# if ket1:
-#     print('Гиря настоящая')
-#
-# print(len(ket1))
-#
-# ket2 = Kettlebell(16)
-# ket3 = Kettlebell(24)
-#
-# print(ket1 == ket2)
-# print(ket1 != ket2)
-#
-# print(ket1 < ket2)
-# print(ket1 <= ket2)
-#
-# print(ket1 > ket2)
-# print(ket1 >= ket2)
-#
-#
-# dumb1 = Dumbbells()
-#
-# # print(ket1 == dumb1)
-#
-# ket4 = Kettlebell(36)
-# ket5 = Kettlebell(42)
-#
-# ket_list = [ket5, ket1, ket3, ket2, ket4]
-# ket_list.sort(reverse=True)
-# [print(k) for k in ket_list]
+class GameManager:
+    def __init__(self):
+        self.Cities = Cities()  # Это Композиция - вызывание классов
+        self.Game = Game(self.Cities)
 
-# Практика
-
-@total_ordering
-class City:
-    def __init__(self, name, population):
-        self.name = name
-        self.population = population
-
-    def __str__(self):
-        return f'Город: {self.name}, Население: {self.population}'
-
-    def __eq__(self, other: 'City') -> bool:  # other - другой экземпляр этого класса
-        if not isinstance(other, City):
-            return NotImplemented
-        return self.population == other.population
-
-    def __lt__(self, other: 'City') -> bool:
-        if not isinstance(other, City):
-            return NotImplemented
-        return self.population < other.population
+    def __call__(self, name):  # Делает объект колобол типа, который можно вызывать
+        print(f'Привет, {name}!')
+        self.Game.print_cities()
 
 
-city1 = City('Москва', 15000000)
-city2 = City('Санкт-Петербург', 11000000)
-city3 = City('Екатеринбург', 8000000)
-city4 = City('Самара', 4000000)
-city5 = City('Петрозаводск', 350000)
-
-print(f'Сравнение на "равенство" Москвы и Питера: {city1 == city2}')
-print(f'Сравнение на "неравенство" Самары и Петрозаводска: {city4 != city5}')
-
-print(f'Сравнение на "больше" Москвы и Питера: {city1 > city2}')
-print(f'Сравнение на "меньше" Екатеринбурга и Петрозаводска: {city3 < city5}')
-
-print(f'Сравнение на "больше и равно" Москвы и Екатеринбурга: {city1 >= city3}')
-print(f'Сравнение на "меньше и равно" Питера и Самары: {city2 <= city4}')
-
-city_list = [city2, city5, city1, city4, city3]
-city_list.sort()
-[print(city) for city in city_list]
+if __name__ == '__main__':
+    name = input('Введите ваше имя: ')
+    game_manager = GameManager()
+    game_manager(name)
