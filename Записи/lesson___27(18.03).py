ef64177d8713dfaef5382eebda396ab9b74d4bbd
Vitaliy <Vitaliy@yandex.ru>
2024-04-04 12:41:32 +0300

    lesson___27(18.03) - практика

--- Записи/lesson___27(18.03).py
+++ Записи/lesson___27(18.03).py
@@ -50,3 +50,28 @@ p1.hobbies.append('программирование')
 p2.hobbies.append('JS')
 print(f'{p1=}')
 print(f'{p2=}')
+
+
+@dataclass
+class City:
+    name: str
+    population: int
+    lat: float = field(compare=False)
+    lon: float = field(compare=False)
+    region: str = field(compare=False)
+
+
+city1 = City('Москва', 15_000_000, 34.234234, 45.1231231, 'Европейский')
+city2 = City('Санкт-Петербург', 11_000_000, 24.234234, 35.1231231, 'Европейский')
+city3 = City('Москва', 15_000_000, 10.2567234, 55.1234231, 'Европейский')
+city4 = City('Петрозаводск', 9_000_000, 21.76674234, 33.12657231, 'Европейский')
+
+print(f'{city1=}')
+print(f'{city2=}')
+print(f'{city3=}')
+print(f'{city4=}')
+
+print(f'Сравнение Москвы населением: {city1.population} с Москвой населением: {city1.population}: {city1 == city3}')
+print(f'Сравнение Москвы населением: {city1.population} с Питером населением: {city2.population}: {city1 == city2}')
+print(f'Сравнение Петрозаводска населением: {city4.population} с Москвой населением: {city1.population}: {city4 == city1}')
+print(f'Сравнение Петрозаводска населением: {city4.population} с Питером населением: {city2.population}: {city4 == city2}')
