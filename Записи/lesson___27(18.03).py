b73620728443e7ee7331b14b4e73d37ff8ee9697
Vitaliy <Vitaliy@yandex.ru>
2024-04-04 11:17:38 +0300

    lesson___27(18.03) - основы датаклассов

--- Записи/lesson___27(18.03).py
+++ Записи/lesson___27(18.03).py
@@ -1,3 +1,37 @@
 """
-Датаклассы
+Lesson 27
+18.03.2024
+Разбор hw_35
+Dataclasses
+- как определяется repr
+- как определяется eq
+- сериализация и десериализация
+- field(default_factory=list) - для изменяемых типов
+- field(compare=False) - для исключения из сравнения
+- __post_init__ - для валидации
+- fields(default = 0) - Значение по умолчанию
+
+СЕРИАЛИЗАЦИЯ - упрощение, то есть превращение экземпляра в строку
+ДЕСЕРИАЛИЗАЦИЯ - усложнение, превращение экземпляра из строки в объект
+
 """
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Person:
+    name: str
+    age: int
+    city: str
+
+
+p1 = Person('Вася', 20, 'Москва')
+p2 = Person('Маша', 18, 'Санкт-Петербург')
+p3 = Person('Вася', 20, 'Москва')
+p4 = Person('Вася', 21, 'Москва')
+
+p1_str = str(p1)
+p1_new = eval(p1_str)
+
+print(p1 == p3)
