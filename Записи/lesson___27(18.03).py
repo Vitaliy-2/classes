54bbb92faa56d3c6ebc8b2b51fada03b9d9012ec
Vitaliy <Vitaliy@yandex.ru>
2024-04-04 12:08:32 +0300

    lesson___27(18.03) - post_init - второй инициализатор

--- Записи/lesson___27(18.03).py
+++ Записи/lesson___27(18.03).py
@@ -17,6 +17,7 @@ Dataclasses
 """
 
 from dataclasses import dataclass, field
+from typing import List
 
 
 @dataclass
@@ -25,16 +26,27 @@ class Person:
     Уберем name и city из сравнения
     """
     name: str
-    age: int = field(compare=False)  # Данная переменная не будет участвовать в сравнении __eq__
     city: str = field(compare=False)
+    hobbies: List[str] = field(default_factory=list)  # Чтобы корректно добавлять данные в список
+    age: int = field(compare=False, default=0)  # Данная переменная не будет участвовать в сравнении __eq__
+
+    def __post_init__(self):
+        self.age = self.validate_age(self.age)
+
+    @staticmethod
+    def validate_age(age):
+        if not isinstance(age, int):
+            raise TypeError('Возраст должен быть числом')
+        if age < 0:
+            raise TypeError('Возраст не может быть отрицательным')
+        if age > 150:
+            raise TypeError('Возраст не может быть больше 150')
+        return age
 
 
-p1 = Person('Вася', 20, 'Архангельск')
-p2 = Person('Маша', 18, 'Санкт-Петербург')
-p3 = Person('Вася', 20, 'Москва')
-p4 = Person('Вася', 21, 'Москва')
-
-p1_str = str(p1)
-p1_new = eval(p1_str)
-
-print(p1 == p4)
+p1 = Person('Иван', 'Москва', ['плавание', 'путешествия'], 35)
+p2 = Person('Анна', 'Санкт-Петербург', ['рафтинг', 'манга'])
+p1.hobbies.append('программирование')
+p2.hobbies.append('JS')
+print(f'{p1=}')
+print(f'{p2=}')
