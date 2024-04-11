47267c815ba6017b97e36915cb89415255a6a64c
Vitaliy <Vitaliy@yandex.ru>
2024-04-11 16:54:16 +0300

    lesson___28(20.03) - работа с локальной страницей

--- Записи/lesson___28(20.03).py
+++ Записи/lesson___28(20.03).py
@@ -63,7 +63,7 @@ SETTINGS = [
 driver = get_driver(SETTINGS)
 
 # Переходим на сайт
-driver.get("http://books.toscrape.com/")
+# driver.get("http://books.toscrape.com/")
 
 # Ищем все товарные карточки на странице
 # article class product_pod
@@ -80,56 +80,65 @@ book_marks = {
     'Four': 4,
     'Five': 5,
 }
-result = []
-count = 0
-while True:
-    # Нашли все книги на странице
-    books = driver.find_elements(By.CLASS_NAME, "product_pod")
+# result = []
+# count = 0
+# while True:
+#     # Нашли все книги на странице
+#     books = driver.find_elements(By.CLASS_NAME, "product_pod")
+#
+#     # Спарсили инфу на этой странице
+#     for book in books:
+#         # требуется h3 в котором находится a и там title
+#         title = book.find_element(By.CSS_SELECTOR, "h3 > a").get_attribute("title")
+#         text_mark = book.find_element(By.CSS_SELECTOR, "p[class^='star-rating']").get_attribute("class").split()[1]
+#         mark = book_marks.get(text_mark)
+#         # print(mark)
+#         # Цена по имени класса price_color
+#         price_str = book.find_element(By.CLASS_NAME, "price_color").text
+#         price = float(price_str.lstrip('£'))
+#         # print(f'{title} - {price}')
+#         # Найдем наличие в продаже. Div product_price > p.instock.availability
+#         in_stock_str = book.find_element(By.CSS_SELECTOR, "div.product_price").find_elements(By.TAG_NAME, "p")[1].text
+#         # print(in_stock_str)
+#         in_stock = 'Есть в наличии' if in_stock_str == "In stock" else False
+#         # print(in_stock)
+#
+#         # img_url - src тега img
+#         row_img_url = book.find_element(By.TAG_NAME, "img").get_attribute("src")
+#         img_url = row_img_url.lstrip('..')
+#         # print(img_url)
+#
+#
+#         result.append({
+#             "title": title,
+#             "mark": mark,
+#             "price": price,
+#             "in_stock": in_stock,
+#             "img_url": img_url,
+#         })
+#
+#
+#     # Пробуем найти кнопку a с текстом Next
+#     try:
+#         next_page = driver.find_element(By.LINK_TEXT, "next")
+#         next_page.click()
+#         time.sleep(1)
+#     except NoSuchElementException:
+#         break
+#     count += 1
+#
+# # Сохраняем результат в файл json
+# with open('books.json', 'w', encoding='utf-8') as file:
+#     json.dump(result, file, ensure_ascii=False, indent=4)
+#
+# pprint(result, sort_dicts=False)
+# print(count)
 
-    # Спарсили инфу на этой странице
-    for book in books:
-        # требуется h3 в котором находится a и там title
-        title = book.find_element(By.CSS_SELECTOR, "h3 > a").get_attribute("title")
-        text_mark = book.find_element(By.CSS_SELECTOR, "p[class^='star-rating']").get_attribute("class").split()[1]
-        mark = book_marks.get(text_mark)
-        # print(mark)
-        # Цена по имени класса price_color
-        price_str = book.find_element(By.CLASS_NAME, "price_color").text
-        price = float(price_str.lstrip('£'))
-        # print(f'{title} - {price}')
-        # Найдем наличие в продаже. Div product_price > p.instock.availability
-        in_stock_str = book.find_element(By.CSS_SELECTOR, "div.product_price").find_elements(By.TAG_NAME, "p")[1].text
-        # print(in_stock_str)
-        in_stock = 'Есть в наличии' if in_stock_str == "In stock" else False
-        # print(in_stock)
 
-        # img_url - src тега img
-        row_img_url = book.find_element(By.TAG_NAME, "img").get_attribute("src")
-        img_url = row_img_url.lstrip('..')
-        # print(img_url)
+# Пробуем открыть локальный html файл
+# Открываем файл через абсолютную ссылку
+FILE = r'C:\Users\123\PycharmProjects\LessonPy\data\Books.htm'
 
+driver.get(f'file://{FILE}')
+time.sleep(10)
 
-        result.append({
-            "title": title,
-            "mark": mark,
-            "price": price,
-            "in_stock": in_stock,
-            "img_url": img_url,
-        })
-
-
-    # Пробуем найти кнопку a с текстом Next
-    try:
-        next_page = driver.find_element(By.LINK_TEXT, "next")
-        next_page.click()
-        time.sleep(1)
-    except NoSuchElementException:
-        break
-    count += 1
-
-# Сохраняем результат в файл json
-with open('books.json', 'w', encoding='utf-8') as file:
-    json.dump(result, file, ensure_ascii=False, indent=4)
-
-pprint(result, sort_dicts=False)
-print(count)
